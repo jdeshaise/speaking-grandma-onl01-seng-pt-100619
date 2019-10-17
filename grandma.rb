3433cf22b46ca6c57ec26ffd7698a5b5eca1eda0
Joseph Miller <jdworldtv@gmail.com>
2019-10-17 18:28:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def "speak_to_grandma method"
+def speak_to_grandma method
   "where are you grandma?"
   
   if "grandma can not hear me"
